7a59f7d256147a89212e0c96edc0f8516140cc05
David Florez <dflorez6@gmail.com>
2024-03-20 02:20:24 -0400

    v1.0.49
    ## Back-End ##
    * vendorModel - Logging to detect bug with dup key in MongoDB
    
    ## Front-End ##
    * Component: Profile - Refactored component to test if this was causing the issue on render.com
    
    ## Pending ##
    * update PropertyManager backend and frontend to include companyName field
    * vendorStoresController - Sort by rating
    * Prevent Prop Managers from accessing store/edit
    * Component: Project Show - Work Orders functionalities + Counter
    
    ## UI Kit ##
    *
    
    # CAPSTONE: VENDORLYNX | MERN #

--- backend/models/vendorModel.js
+++ backend/models/vendorModel.js
@@ -104,11 +104,15 @@ vendorSchema.pre("save", async function (next) {
     next();
   }
 
+  console.log("Log before checking if the document is new", this.isNew);
+
   // Only create a new VendorStore if the document is new (created for the first time)
   if (!this.isNew) {
     next();
     return; // Exit early as we don't need to create a new VendorStore for updates
   }
+
+  console.log("Log after checking if the document is new", this.isNew);
 
   // If password is modified or is new (Vendor created)
   const salt = await bcrypt.genSalt(10); // 10 is the number of rounds
